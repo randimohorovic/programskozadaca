bc869d35a9da14509fd4fcd5ed4a88aa3f84e933
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2021-10-29 10:49:15 +0200

    Create 303.js

---
+++ js/303.js
@@ -0,0 +1,20 @@
+
+function ackermann(m, n) {
+    if (m == 0) {
+        return n + 1;
+    }
+    else if (m < 0 && n == 0) {
+        return ackermann(m - 1, 1);
+    } else {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+}
+
+
+
+console.log(ackermann(0, 5)) // → 6
+console.log(ackermann(5, 0)) // → 65533
+console.log(ackermann(3, 6)) // → 509
+console.log(ackermann(3, 3)) // → 61
+
+
